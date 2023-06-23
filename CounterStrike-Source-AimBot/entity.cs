a7a49e4cfb98c37064de3b55487d5ee19e467696
Erenn <60568960+sadece1eren@users.noreply.github.com>
2023-06-23 16:24:45 +0300

    Create entity.cs

---
+++ CounterStrike-Source-AimBot/entity.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterStrike_Source_AimBot
+{
+        public class entity
+        {
+            public int health, team;
+            public float x, y, z, mag;
+        }
+}
